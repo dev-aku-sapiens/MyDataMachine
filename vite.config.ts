83783e0d634bfd860908b010649dbed1b03bde63
Adhavakkannan <adhavakkannan@pikcha.com>
2024-05-14 21:57:48 +0530

    update remove - pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-      ],
-      manifest: {
-        name: 'My React App',
-        short_name: 'ReactApp',
-        description: 'My React Application',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
